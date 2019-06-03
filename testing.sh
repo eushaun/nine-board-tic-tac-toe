27c26b8fde5e12ab5050357b6813ec81a66bdaa9
eushaun <31348290+eushaun@users.noreply.github.com>
2019-06-03 23:24:41 +1000

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -2,13 +2,14 @@
 
 # Play agent against specified program 100 times
 # Example:
-# ./playc.sh lookt 12345
+# ./playpy.sh lookt 12345
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <player> <port>" >&2
   exit 1
 fi
 
+# p is the port number
 p=11300
 i=0
 while ((i < 100))
